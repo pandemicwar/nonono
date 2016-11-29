3c29c1d6d9dc12afa35924c34fc98b7349dd1779
pandemicwar <VasilevVS007@outlook.com>
2016-11-29 22:03:47 +0400

    Update open.cpp

--- open.cpp
+++ open.cpp
@@ -1,13 +1,4 @@
-#include<iostream>
-    using namespace std;
-    int main()
-    {
-       int range, first = 0, second = 1, fibonicci=0;
-       cout << "Enter Range for Terms of Fibonacci Sequence: ";
-       cin >> range;
-       cout << "Fibonicci Series upto " << range << " Terms "<< endl;
-       for ( int c = 0 ; c < range ; c++ )
-       {
+
           if ( c <= 1 )
              fibonicci = c;
           else
